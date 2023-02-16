34f67ad31162fff1a8de85e201f3acddc86fdd42
amrkhaleds <amrkhaleds898@gmail.com>
2023-02-16 09:16:01 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void){
+/**
+ * main - use puts function that return,
+ * string with new line
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
