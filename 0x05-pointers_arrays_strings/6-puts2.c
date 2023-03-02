7fdaf33b732a7496f03dfd151d6eb5e022d9b2a9
amrkhaleds <amrkhaleds898@gmail.com>
2023-03-02 03:47:26 -0800

    6-puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other char of a string,
+ * @str: param
+ *
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
