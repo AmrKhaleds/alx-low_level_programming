49fc27b962940844587ed680bc9eb518927965de
root <Amr Khaled>
2023-07-06 01:49:27 -0700

    --

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _puts_recursion : function that prints a string using recursion
- * @s
+ * _puts_recursion - function that prints a string using recursion
+ * @s : char parameter
  *
  * Return: void function that not return anything
  */
