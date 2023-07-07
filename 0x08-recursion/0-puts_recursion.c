68197f36e7a012bca3044c3c0af0cb3e55515521
root <Amr Khaled>
2023-07-06 19:38:12 -0700

    -

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,15 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * _puts_recursion - function that prints a string using recursion
- * @s : char parameter
+ * _puts_recursion - function that fills memory with a constant byte
+ *@s: first value
  *
- * Return: void function that not return anything
  */
 void _puts_recursion(char *s)
 {
+
 	if (s[0] == '\0')
-	{
 		_putchar('\n');
-	} else
+	else
 	{
 		_putchar(s[0]);
 		s++;
