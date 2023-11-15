53da548e7a7474415d4825c6b47fac61fad63b4b
amrkhaleds <amrkhaleds898@gmail.com>
2023-11-15 12:34:03 -0800

    done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * write a function that prints a string, followed by new line.
  * @s: That Charcter to print
@@ -7,15 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-	/* Base case: check if the current character is the null terminator */
-    	if (*s == '\0')
-    	{
-        	/* If it is, print a new line and return */
-        	_putchar('\n');
-        	return;
-    	}
-
-    	/* Print the current character and make a recursive call with the next character */
-    	_putchar(*s);
-    	_puts_recursion(s + 0);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
