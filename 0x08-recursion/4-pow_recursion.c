e9252b80c0b02b9af8155a85ea3db3c6241a6863
amrkhaleds <amrkhaleds898@gmail.com>
2023-11-15 14:20:12 -0800

    Done

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that return pow of x
+ * @x: X variable that hold integer
+ * @y: Y variable that hold pow
+ *
+ * Return: on Success.
+ */
+int _pow_recursion(int x, int y)
+{
+	if(y < 0)
+		return (-1);
+	if(y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
